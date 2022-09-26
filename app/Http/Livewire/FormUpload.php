40c6e418464fb9a10c6c4ddd48df82e68c3c4f07
andre-arc <andridarnius@gmail.com>
2022-09-26 10:43:36 +0700

    add mount() & add Model to Component

--- app/Http/Livewire/FormUpload.php
+++ app/Http/Livewire/FormUpload.php
@@ -9,10 +9,21 @@ use Filament\Forms\Contracts\HasForms;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\MarkdownEditor;
 use Livewire\Component;
+use App\Models\Post;
 
 class FormUpload extends Component implements HasForms
 {
     use InteractsWithForms;
+
+    public Post $post;
+
+    public $title;
+    public $content;
+    public $image;
+
+    public function mount(){
+        $this->form->fill();
+    }
 
     public function render()
     {
@@ -24,9 +35,14 @@ class FormUpload extends Component implements HasForms
         return [
             TextInput::make('title')->required(),
             MarkdownEditor::make('content'),
-            // FileUpload::make('image')->avatar()
+            FileUpload::make('image')->avatar()
 
-            SpatieMediaLibraryFileUpload::make('avatar')->avatar(),
+            // SpatieMediaLibraryFileUpload::make('avatar')->avatar(),
         ];
+    }
+
+    protected function getFormModel(): string
+    {
+        return Post::class;
     }
 }
